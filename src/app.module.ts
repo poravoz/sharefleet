918fd6dfc47f2348162905136934dd1d57276a87
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-11 15:26:12 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< Updated upstream
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import { DatabaseModule } from './database/database.module';
@@ -10,8 +9,6 @@ import { SubscribersController } from './subscribers/subscribers.controller';
 import { SubscribersService } from './subscribers/subscribers.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Subscriber from './subscribers/subscriber.entity';
-=======
->>>>>>> Stashed changes
 
 @Module({
   imports: [],
