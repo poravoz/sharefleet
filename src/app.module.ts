ba39730f9ad4a0aa9e44af290f1cf7d3f0cb17ac
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-12 14:46:14 +0300

    package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ApiModule],
+  controllers: [ApiController],
+  providers: [],
 })
 export class AppModule {}
