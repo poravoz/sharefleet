c8fac6ef428ae3e8c8563b472c2694b145f17447
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-12 14:12:31 +0300

    microservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
-import { DatabaseModule } from './database/database.module';
-import { SubscribersModule } from './subscribers/subscribers.module';
-import { SubscribersController } from './subscribers/subscribers.controller';
-import { SubscribersService } from './subscribers/subscribers.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import Subscriber from './subscribers/subscriber.entity';
 
 @Module({
   imports: [],
--- src/subscribers/dto/createSubscriber.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateSubscriberDto {
-  email: string;
-  name: string;
-}
-
-export default CreateSubscriberDto;
