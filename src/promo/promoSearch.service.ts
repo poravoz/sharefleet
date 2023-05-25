4eac572582d48daf006be8409a4008ecb5f7e96f
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-25 15:31:45 +0300

    Update promoSearch.service.ts

--- src/promo/promoSearch.service.ts
+++ src/promo/promoSearch.service.ts
@@ -3,7 +3,7 @@ import { ElasticsearchService } from '@nestjs/elasticsearch';
 import { PromoCode } from './dto/promo.dto';
 import promoCodeSearchResult from './types/postSearchResponse.interface';
 import PromoCodeSearchBody from './types/promoSearchBodyInterface';
-
+import { Client } from '@elastic/elasticsearch';
  
 @Injectable()
 export class PromoSearchService {
@@ -12,29 +12,35 @@ export class PromoSearchService {
   constructor(
     private readonly elasticsearchService: ElasticsearchService
   ) {}
- 
-  async search(text: string) { 
-    console.log(text);
 
-    const body = await this.elasticsearchService.search({
-      
-      index: this.index,
-      body: {
-        query: {
-          multi_match: {
-            query: text,
-            fields: ["code"]
+  async search(text) {
+    try {
+      const body = await this.elasticsearchService.search({
+        index: this.index,
+        body: {
+          query: {
+            multi_match: {
+              query: text,
+              fields: "code",
+            }, 
           },
         },
-      },
-    });
-    console.log(body);
-    const hits = body.hits.hits;
-    const results = new Set();
-    hits.map((item) => {
-      results.add(item._source as PromoCodeSearchBody); 
-    });
-    return Array.from(results);
+      });
+      
+      console.log(body);
+
+      const hits = body.hits.hits;
+      const results = new Set();
+      hits.map((item) => {
+        results.add(item._source as PromoCodeSearchBody); 
+      });
+      
+      return Array.from(results);
+    } catch (error) {
+      console.error("Ошибка при выполнении поиска промо-кодов:", error);
+      throw error;
+    }
   }
 }
+
 
