85fa45bef7903801f9ccad8e5a959732d91073be
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-29 18:24:36 +0300

    1

---
+++ src/front-end/scriptForFaceRecognation.js
@@ -0,0 +1,19 @@
+const video = document.getElementById('video')
+var faceapi = 'face-api.min.js';
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognationNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo)
+
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+startVideo()
