6444b4b8b69f71cac5321f6cd8a3e5ae5533fa20
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-31 12:15:03 +0300

    1

--- src/face/face.service.ts
+++ src/face/face.service.ts
@@ -4,7 +4,7 @@ import { Body } from '@nestjs/common';
 @Injectable()
 export class FaceControllerService {
   handleFaceCoordinates(@Body() faceCoordinates: number) {
-    //console.log('Received face coordinates:', faceCoordinates);
+    console.log('Received face coordinates:', faceCoordinates);
     return 'Face coordinates received';
   }
 }
--- src/front-end/script.js
+++ src/front-end/script.js
@@ -2,94 +2,94 @@ const imageUpload = document.getElementById('fileInput');
 const uploadButton = document.getElementById('uploadButton');
 const imageContainer = document.getElementById('imageContainer');
 let images = [];
-let currentPhotoIndex = 0;
 
 Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
 ]).then(() => {
-    uploadButton.addEventListener('click', async () => {
-        const file = imageUpload.files[0];
-      
-        if (file) {
-          const formData = new FormData();
-          formData.append("file", file);
-      
-          try {
-            const response = await fetch("http://localhost:5433/user/uploadfile", {
-              method: "POST",
-              body: formData,
-            });
-      
-            const data = await response.json();
-            const imageUrl = data.url;
-            const fileId = data.id;
-      
-            if (images.length >= 4) {
-              console.log("Max file count reached");
-              return;
-            }
-      
-            const imageElement = document.createElement("img");
-            imageElement.src = imageUrl;
-      
-            const deleteButton = document.createElement("button");
-            deleteButton.innerText = "Delete File";
-            deleteButton.classList.add("deleteButton");
-            deleteButton.setAttribute("data-id", fileId);
-            deleteButton.addEventListener("click", function () {
-              const clickedId = this.getAttribute("data-id");
-              deleteImage(clickedId);
-            });
-      
-            const container = document.createElement("div");
-            container.appendChild(imageElement);
-            container.appendChild(deleteButton);
-            imageContainer.appendChild(container);
-      
-            const image = await faceapi.bufferToImage(file);
-            const canvas = faceapi.createCanvasFromMedia(image);
-            const displaySize = { width: image.width, height: image.height };
-            faceapi.matchDimensions(canvas, displaySize);
-            const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
-            const resizedDetections = faceapi.resizeResults(detections, displaySize);
-            resizedDetections.forEach(detection => {
-              const box = detection.detection.box;
-              const drawBox = new faceapi.draw.DrawBox(box, { label: 'Darkness man' });
-              drawBox.draw(canvas);
-      
-              const center = {
-                x: box.x + box.width / 2,
-                y: box.y + box.height / 2
-              };
-              console.log('Face center coordinates:', center);
-      
-            });
-            canvas.classList.add('face-api-canvas');
-            container.appendChild(canvas);
-      
-            const maskDataURL = canvas.toDataURL();
-            const maskImage = document.createElement('img');
-            maskImage.src = maskDataURL;
-      
-            const newImage = {
-              url: imageUrl,
-              id: fileId,
-              mask: maskDataURL
-            };
-      
-            images.push(newImage);
-            currentPhotoIndex++;
-      
-          } catch (error) {
-            console.error("Error: ", error);
-          }
-        } else {
-          console.error("No file selected");
+  uploadButton.addEventListener('click', async () => {
+    const file = imageUpload.files[0];
+  
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+  
+      try {
+        const response = await fetch("http://localhost:5433/user/uploadfile", {
+          method: "POST",
+          body: formData,
+        });
+  
+        const data = await response.json();
+        const imageUrl = data.url;
+        const fileId = data.id;
+  
+        if (images.length >= 4) {
+          console.log("Max file count reached");
+          return;
         }
-      });
+  
+        const imageElement = document.createElement("img");
+        imageElement.src = imageUrl;
+  
+        const deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete File";
+        deleteButton.classList.add("deleteButton");
+        deleteButton.setAttribute("data-id", fileId);
+        deleteButton.addEventListener("click", function () {
+          const clickedId = this.getAttribute("data-id");
+          deleteImage(clickedId);
+        });
+  
+        const container = document.createElement("div");
+        container.appendChild(imageElement);
+        container.appendChild(deleteButton);
+        imageContainer.appendChild(container);
+  
+        const image = await faceapi.bufferToImage(file);
+        const canvas = faceapi.createCanvasFromMedia(image);
+        const displaySize = { width: image.width, height: image.height };
+        faceapi.matchDimensions(canvas, displaySize);
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        resizedDetections.forEach(async (detection) => {
+          const box = detection.detection.box;
+          const drawBox = new faceapi.draw.DrawBox(box, { label: 'Darkness man' });
+          drawBox.draw(canvas);
+        
+          const faceCoordinates = extractFaceCoordinates(detection);
+          await sendFaceCordinateToBackend(faceCoordinates); 
+            
+        });
+        canvas.classList.add('face-api-canvas');
+        container.appendChild(canvas);
+  
+        const maskDataURL = canvas.toDataURL();
+        const maskImage = document.createElement('img');
+        maskImage.src = maskDataURL;
+  
+        const newImage = {
+          url: imageUrl,
+          id: fileId,
+          mask: maskDataURL
+        };
+  
+        images.push(newImage);
+  
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    } else {
+      console.error("No file selected");
+    }
+  });
 });
+
+function extractFaceCoordinates(detection) {
+  const { x, y, width, height } = detection.detection.box;
+  return { x, y, width, height };
+}
 
 function loadImagesFromStorage() {
     const imagesData = localStorage.getItem('images');
@@ -148,3 +148,37 @@ function deleteImage(id) {
         maskImage.remove();
     }
 }
+
+function sendFaceCordinateToBackend(data) {
+  const faceType = determineFaceType(data);
+  const payload = {
+    ...data,
+    type: faceType,
+  };
+
+  fetch('http://localhost:5433/api/face-coordinates', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(payload)
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Face coordinates sent to the backend successfully');
+      } else {
+        console.error('Failed to send face coordinates to the backend');
+      }
+    })
+    .catch(error => {
+      console.error('Error sending face coordinates to the backend:', error);
+    });
+}
+
+function determineFaceType(data) {
+  const { width, height } = data;
+
+  const isEllipse = width / height < 0.8;
+  return isEllipse ? 'ellipse' : 'vertical rectangle';
+}
+
