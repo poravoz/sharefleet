b5ebfd624ed2c826b7dc2027f8c39a770e930be4
poravoz <116353344+poravoz@users.noreply.github.com>
2023-05-31 21:05:52 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,24 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type',
   });
 
+  const amqp = require("amqplib");
+  async function connect() {
+  try {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    await channel.assertQueue("number");
+    channel.consume("number", message => {
+      const input = JSON.parse(message.content.toString());
+      console.log(`Received number: ${input.number}`);
+      channel.ack(message);
+    });
+    console.log(`Waiting for messages...`);
+  } catch (ex) {
+    console.error(ex);
+  }
+}
+connect();
+
   app.startAllMicroservices();
   await app.listen(5433);
 }
