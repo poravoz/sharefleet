b245b30afbdb805837c8d06f44c3f4f1736488b8
poravoz <116353344+poravoz@users.noreply.github.com>
2023-06-19 15:52:54 +0300

    1

--- src/schedule/front-end/assets/js/script.js
+++ src/schedule/front-end/assets/js/script.js
@@ -1,24 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
-    createScheduleTables();
-    restoreScheduleFromStorage();
-});
+fetch('http://localhost:5433/schedule/')
+  .then(response => response.json())
+  .then(data => {
+    const table = document.createElement('table');
 
-function createScheduleTables() {
-    var daysOfWeek = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\`ятниця'];
-    var container = document.getElementById('scheduleTablesContainer');
+    const thead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    Object.keys(data[0]).forEach(key => {
+      if (key !== 'id') {
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
+      }
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
 
-    for (var i = 0; i < daysOfWeek.length; i++) {
-        var table = document.createElement('table');
-        table.id = 'scheduleTable_' + i;
-        var tableContent = '<tr><th colspan="5">' + daysOfWeek[i] + '</th></tr><tr><th>№ пари</th><th>Час</th><th>Предмет</th><th>Викладач</th><th>Аудиторія-посилання</th></tr>';
-
-        for (var j = 1; j <= 5; j++) {
-            tableContent += '<tr><td style="width: 25px; text-align: center;">' + j + '</td><td contenteditable="true"></td><td contenteditable="true"></td><td contenteditable="true"></td><td contenteditable="true"></td></tr>';
+    const tbody = document.createElement('tbody');
+    data.forEach(item => {
+      const row = document.createElement('tr');
+      Object.entries(item).forEach(([key, value]) => {
+        if (key !== 'id') {
+          const cell = document.createElement('td');
+          if (Array.isArray(value)) {
+            const valuesContainer = document.createElement('div');
+            value.forEach(item => {
+              const valueElement = document.createElement('div');
+              valueElement.textContent = item;
+              valuesContainer.appendChild(valueElement);
+            });
+            cell.appendChild(valuesContainer);
+          } else {
+            cell.textContent = value;
+          }
+          row.appendChild(cell);
         }
+      });
+      tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
 
-        table.innerHTML = tableContent;
-        container.appendChild(table);
-    }
-}
-
-
+    document.body.appendChild(table);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
