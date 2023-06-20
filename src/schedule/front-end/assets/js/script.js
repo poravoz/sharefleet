fcb9257e1ae33949a1aedaab9d18c307de66fdd4
poravoz <116353344+poravoz@users.noreply.github.com>
2023-06-20 20:27:14 +0300

    1

--- src/schedule/front-end/assets/js/script.js
+++ src/schedule/front-end/assets/js/script.js
@@ -18,7 +18,7 @@ fetch('http://localhost:5433/schedule/')
 
     const tbody = document.createElement('tbody');
 
-    let rowIndex = 0; // Счетчик строк
+    let rowIndex = 0; 
 
     data.forEach((item) => {
       const timeArray = item.time;
@@ -35,7 +35,7 @@ fetch('http://localhost:5433/schedule/')
           const dayOfWeekCell = document.createElement('td');
           dayOfWeekCell.textContent = item.dayOfWeek;
           dayOfWeekCell.style.textAlign = 'center';
-          const rowspan = maxLength - i; // Вычисляем правильное значение rowspan
+          const rowspan = maxLength - i; 
           dayOfWeekCell.rowSpan = rowspan;
           row.appendChild(dayOfWeekCell);
         }
@@ -51,6 +51,7 @@ fetch('http://localhost:5433/schedule/')
           if (subjects.length === 1) {
             subjectsCell.textContent = subjects[0].trim();
           } else if (subjects.length === 2) {
+            
             const topText = subjects[0].trim();
             const bottomText = subjects[1].trim();
 
@@ -60,15 +61,54 @@ fetch('http://localhost:5433/schedule/')
 
             subjectsCell.appendChild(document.createElement('br'));
 
+            const divider = document.createElement('div');
+            divider.style.borderTop = '2px solid #000'; 
+            divider.style.margin = '4px 0';
+            subjectsCell.style.paddingLeft = '0px';
+            subjectsCell.style.paddingRight = '0px'
+            
+            subjectsCell.appendChild(divider);
+
             const bottomSpan = document.createElement('span');
             bottomSpan.textContent = bottomText;
             subjectsCell.appendChild(bottomSpan);
           }
+
+          subjectsCell.style.borderRight = '1px solid #000'; 
         }
         row.appendChild(subjectsCell);
 
         const teachersCell = document.createElement('td');
-        teachersCell.textContent = i < teachersArray.length ? teachersArray[i] : '';
+        if (i < teachersArray.length) {
+          const teachers = teachersArray[i].split('/');
+
+          if (teachers.length === 1) {
+            teachersCell.textContent = teachers[0].trim();
+          } else if (teachers.length === 2) {
+            const topText = teachers[0].trim();
+            const bottomText = teachers[1].trim();
+
+            const topSpan = document.createElement('span');
+            topSpan.textContent = topText;
+            teachersCell.appendChild(topSpan);
+
+            teachersCell.appendChild(document.createElement('br'));
+
+            const divider = document.createElement('div');
+            divider.className = 'divider';
+            divider.style.borderTop = '2px solid #000'; 
+            divider.style.margin = '4px 0'; 
+            teachersCell.appendChild(divider);
+
+            const bottomSpan = document.createElement('span');
+            bottomSpan.textContent = bottomText;
+            teachersCell.appendChild(bottomSpan);
+          }
+
+          teachersCell.style.borderRight = '1px solid #000'; 
+          teachersCell.style.paddingRight = '0px'; 
+          teachersCell.style.paddingLeft = '0px';
+        }
         row.appendChild(teachersCell);
 
         const classroomLinksCell = document.createElement('td');
@@ -87,7 +127,7 @@ fetch('http://localhost:5433/schedule/')
 
         tbody.appendChild(row);
 
-        rowIndex++; // Увеличиваем счетчик строк
+        rowIndex++; 
       }
     });
     table.appendChild(tbody);
