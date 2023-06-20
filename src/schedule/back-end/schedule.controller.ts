971af4d9b167a3d55622002f5ee484e76d406f2e
poravoz <116353344+poravoz@users.noreply.github.com>
2023-06-20 15:01:45 +0300

    Update schedule.controller.ts

--- src/schedule/back-end/schedule.controller.ts
+++ src/schedule/back-end/schedule.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Delete, Body, Param } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { Schedule } from './schedule.entity';
-import {v4 as uuidv4 } from 'uuid'
 
 @Controller('schedule')
 export class ScheduleController {
@@ -14,7 +13,6 @@ export class ScheduleController {
 
   @Post()
   async createSchedules(@Body() createScheduleDtoArray: Schedule[]): Promise<Schedule[]> {
-    const id = uuidv4();
     return this.scheduleService.createSchedules(createScheduleDtoArray);
   }
 
