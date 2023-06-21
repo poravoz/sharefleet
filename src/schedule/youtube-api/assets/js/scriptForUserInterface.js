9142b74773a5b5363c878adba3f0b52a281f71eb
poravoz <116353344+poravoz@users.noreply.github.com>
2023-06-21 11:29:59 +0300

    1

--- src/schedule/youtube-api/assets/js/scriptForUserInterface.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const viewTable = document.getElementById('viewTable');
-    const savedData = localStorage.getItem('tableData');
-    if (savedData) {
-      viewTable.innerHTML = savedData;
-    }
-  });
-
-
